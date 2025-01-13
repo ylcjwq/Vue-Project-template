635413b7a77b9d7e47c05075c42f74c12ed827f1
ylc <756885686@qq.com>
2025-01-13 15:20:49 +0800

    feat: 挂载pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routers';
+import { createPinia } from 'pinia';
 import 'uno.css';
 
-createApp(App).use(router).mount('#app');
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount('#app');
